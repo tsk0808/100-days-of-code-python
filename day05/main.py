7c8a61a3d39288a1f01d2f2bae26e8f6d1afa2bf
tsk0808 <tsk0808@gmail.com>
2022-07-04 20:03:46 +0900

    [commit] changed day05

--- day05/main.py
+++ day05/main.py
@@ -6,27 +6,27 @@ numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
-nr_letters= (input("How many letters would you like in your password?\n"))
-nr_symbols = (input(f"How many symbols would you like?\n"))
-nr_numbers = (input(f"How many numbers would you like?\n"))
+num_letters= (input("How many letters would you like in your password?\n"))
+num_symbols = (input(f"How many symbols would you like?\n"))
+num_numbers = (input(f"How many numbers would you like?\n"))
 
-if not nr_letters.isdigit() or not nr_symbols.isdigit() or not nr_numbers.isdigit():
+if not num_letters.isdigit() or not num_symbols.isdigit() or not num_numbers.isdigit():
   print("Invalid value, enter a number instead.")
 else:
   password = []
 
   random_letter = random.randint(0, 51)
-  for i in range(0, int(nr_letters)):
+  for i in range(0, int(num_letters)):
     random_letter = random.randint(0, 51)
     password.append(letters[random_letter])
 
   random_number = random.randint(0,9)
-  for i in range(0, int(nr_numbers)):
+  for i in range(0, int(num_numbers)):
     random_number = random.randint(0,9)
     password.append(numbers[random_number])
 
   random_symbol = random.randint(0,8)
-  for i in range(0, int(nr_symbols)):
+  for i in range(0, int(num_symbols)):
     random_symbol = random.randint(0,8)
     password.append(symbols[random_symbol])
 
